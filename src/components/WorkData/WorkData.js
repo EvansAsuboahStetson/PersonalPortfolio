57afa02537645855215e8e9d2c0090050f9ca21c
Evans Asuboah <easuboah@stetson.edu>
2023-07-31 02:13:05 -0700

    Added new ASU experience

--- src/components/WorkData/WorkData.js
+++ src/components/WorkData/WorkData.js
@@ -5,22 +5,22 @@ const ProjectCardData = [
     {
         imgsrc: pro1,
         title: "Alumni Connect",
-        text: "Hey Evans, Looks like your free DashPass Student membership is paying off! Be sure to check out all the ways you can save on delivery with DashPass, thanks to Chegg.",
+        text: "Introducing Alumni Connector, a unique platform designed to bridge the gap between Stetson students and alumni. Recognizing the challenges of connecting beyond LinkedIn, I developed this application to facilitate seamless interactions between students and alumni. With personalized filters based on majors, minors, and interests, students can easily discover and connect with relevant alumni. The application enables smooth communication through a chat feature, fostering meaningful conversations. Moreover, both students and alumni can actively engage by posting events and sharing updates, creating a vibrant community of learning and networking.",
         view: "https://www.youtube.com/watch?v=oJvWCRGjdXA",
-        extraText: "I led a team of four students in developing a MERN stack-based web application that facilitates connections between students and alumni. The project included a chat feature powered by Twilio API. Additionally, I gained valuable experience in product development and attended industry panels with renowned professionals",
+        extraText: "During the project, I spearheaded a team of four talented students to create a cutting-edge web application using the MERN stack. This innovative platform aims to foster meaningful connections between students and alumni. To enhance user engagement, we integrated a powerful chat feature leveraging the Twilio API. Throughout the development process, I honed my skills in product development and had the privilege of participating in industry panels alongside esteemed professionals. The experience was invaluable and enriched my understanding of the tech industry.",
         second_button_title: "Source"
     },
     {
         imgsrc: facade,
         title: "Kinetic Facade Prototype",
-        text: "During my professor's final year of Ph.D., I was chosen to assist him during the summer with his Ph.D. paper. The research focused on a kinetic facade, exploring how sunlight affects people's moods. I contributed by coding the moving components of the facade and collaborating with him to assemble all the parts successfully.",
+        text: "In my professor's last year of pursuing his Ph.D., I had the honor of being selected as his summer assistant for his research paper. The study centered around a kinetic facade, delving into the impact of sunlight on people's moods. My significant contribution to the project involved coding the dynamic components of the facade. Additionally, I worked closely with him to ensure the successful assembly of all the intricate parts, playing a pivotal role in the research's accomplishment",
         view: "https://youtu.be/nj48IkEjLQc",
         second_button_title: "Source"
     },
     {
         imgsrc: Terralearn,
         title: "Terra Learn",
-        text: "Worked with a team of junior engineers to develop a game called Terralearn where in which players are dropped somewhere in the world in a street view panorama and it is the players mission is to find clues and guess their location on the world map.",
+        text: "I collaborated with a team of junior engineers to create an exciting game called Terralearn. In this captivating game, players are immersed in a street view panorama of a random location worldwide. Their thrilling mission is to unravel clues and make educated guesses to pinpoint their exact location on the world map. Together, we brought this unique gaming experience to life, combining creativity and technical expertise to engage players in an immersive and educational adventure.",
         view: "https://youtu.be/ozIahxawHOI",
         second_button_title: "Source"
     },
