948d71fac0a8dfed43833ecf6dcb1dcf1bf2c420
Evans Asuboah <easuboah@stetson.edu>
2023-07-31 02:13:05 -0700

    Added new ASU experience

--- src/routes/Homepage/Homepage.js
+++ src/routes/Homepage/Homepage.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import HeroImage from '../../components/Hero/HeroImage'
 import Footer from '../../components/Footer/Footer'
-import Work from '../../components/Work/Work'
-
 const Homepage = () => {
   return (
     <div>
