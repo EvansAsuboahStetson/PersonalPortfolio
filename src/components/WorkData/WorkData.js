c58c1afad65c906fffa07d9f2e049fd896aebda5
Evans Asuboah <easuboah@stetson.edu>
2023-07-31 02:13:05 -0700

    Added new ASU experience

--- src/components/WorkData/WorkData.js
+++ src/components/WorkData/WorkData.js
@@ -5,7 +5,7 @@ const ProjectCardData = [
     {
         imgsrc: pro1,
         title: "Alumni Connect",
-        text: "Hey Evans, Looks like your free DashPass Student membership is paying off! Be sure to check out all the ways you can save on delivery with DashPass, thanks to Chegg.",
+        text: "Alumni Connector:Connecting stetson students and alumni can be harder sometimes. Aside linkedIn it is harder to connect students and alumni. So I created this  application where students can filter alumns according to majors or minors or interest. Send them a connection. And finally chat with them. I also allow  students and alumns to be able to post events and stuff happening",
         view: "https://www.youtube.com/watch?v=oJvWCRGjdXA",
         extraText: "I led a team of four students in developing a MERN stack-based web application that facilitates connections between students and alumni. The project included a chat feature powered by Twilio API. Additionally, I gained valuable experience in product development and attended industry panels with renowned professionals",
         second_button_title: "Source"
