cb872f306fec1a92647f6c181101c67f06320be1
Evans Asuboah <easuboah@stetson.edu>
2024-06-15 19:31:22 -0400

    new css

--- src/components/GalleryData/GalleryData.js
+++ src/components/GalleryData/GalleryData.js
@@ -9,6 +9,8 @@ import bev from "../../assets/bev.jpg";
 import Georgia from "../../assets/Georgia.jpg";
 import Feedback from "../../assets/Feedback.jpg";
 import BHCF_Award from "../../assets/BHCF_Award.jpg";
+import asu from "../../assets/asu.jpeg"
+import asuPresido from "../../assets/asuPresido.jpeg"
 
 const GalleryData = [
     {
@@ -40,6 +42,10 @@ const GalleryData = [
         description: "African Student Union Send of event at Stetson University",
     },
     {
+        src: asuPresido,
+        description: "African Students Union Award"
+    },
+    {
         src: Georgia,   
         description: "Spent my spring break in Georgia with Habitat for Humanity to build Houses for the homeless",
     },
@@ -53,9 +59,15 @@ const GalleryData = [
         description: "My Maris price award at Stetson University",
     },
     {
+        src: asu,
+        description: "My Stetson Family"
+    },
+    {
         src: BHCF_Award,
         description: "Black Home Schoolers of Central Florida Award",
     },
+
+  
  
 
 ]
